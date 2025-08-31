92e0410e2f5f959cd77c0639419ec849e9b8451d
ylopatkin <ylopatkin@gmail.com>
2025-08-31 14:12:48 +0200

    31.08 - added verification when calling endpoints

--- src/main/java/org/yaroslaavl/cvservice/config/SecurityConfig.java
+++ src/main/java/org/yaroslaavl/cvservice/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package org.yaroslaavl.cvservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+import org.yaroslaavl.cvservice.config.converter.KeyCloakAuthenticationRoleConverter;
+
+import java.util.Collection;
 
 @Configuration
 public class SecurityConfig {
@@ -14,13 +18,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtConfigurer -> jwtConfigurer
+                                .jwtAuthenticationConverter(jwtToken -> {
+                                    Collection<GrantedAuthority> authorities = new KeyCloakAuthenticationRoleConverter().convert(jwtToken);
+                                    return new JwtAuthenticationToken(jwtToken, authorities);
+                                })
+                        )
+                )
                 .authorizeHttpRequests(
                         request -> request
                                 .requestMatchers(
                                         "/error",
                                         "/actuator/health").permitAll()
-                                .requestMatchers("/api/v1/cv/**").authenticated()
+                                .requestMatchers("/api/v1/cv/**").hasRole("VERIFIED_CANDIDATE")
+                                .requestMatchers("/api/v1/cv/*/recruiter").hasRole("INTERNAL_SERVICE")
                 );
 
         return http.build();
